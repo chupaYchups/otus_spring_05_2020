45170ac334f266ff50d7320a41c01f75145facf0
chupaYchups <chupaYchups@gmail.com>
2020-09-16 10:39:55 +0300

    home work 4

---
+++ L04-springBoot/src/main/java/ru/chupaYchups/otus_spring_homework/L04SpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.chupaYchups.otus_spring_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L04SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L04SpringBootApplication.class, args);
+	}
+
+}
