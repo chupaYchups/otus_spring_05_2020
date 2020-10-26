e1b19c9f1dd8f8af65c08b41e06e61bc1cefa4b4
chupaYchups <chupaYchups@gmail.com>
2020-10-26 22:58:25 +0300

    homework 5

--- L05-springShell/src/test/java/ru/chupaYchups/question/component/QuestionsFileNameResolverTest.java
+++ L05-springShell/src/test/java/ru/chupaYchups/question/component/QuestionsFileNameResolverTest.java
@@ -1,51 +1,43 @@
 package ru.chupaYchups.question.component;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.MessageSource;
-import ru.chupaYchups.question.config.ApplicationProps;
-import ru.chupaYchups.question.config.QuestionsFileProps;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import java.util.Locale;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 @DisplayName("Тестирование что")
 class QuestionsFileNameResolverTest {
 
-    @Mock
+    @Configuration
+    @ComponentScan(value = {"ru.chupaYchups.question.component", "ru.chupaYchups.question.config"})
+    static class TestConfiguration {}
+
+    @MockBean
     private MessageSource messageSource;
 
+    @Autowired
     private QuestionsFileNameResolver resolver;
-    private QuestionsFileProps questionsFileProps;
-    private ApplicationProps applicationProps;
 
     private final static String QUESTION_FILE_NAME_RU = "вопросы";
     private final static String QUESTION_FILE_NAME_PROPERTY = "questions";
     private final static String QUESTION_FILE_EXT = "csv";
     private static final String LOCALE_RU = "ru";
 
-    @BeforeEach
-    void setUp() {
-        questionsFileProps = new QuestionsFileProps();
-        questionsFileProps.setNameProperty(QUESTION_FILE_NAME_PROPERTY);
-        questionsFileProps.setExtension(QUESTION_FILE_EXT);
-
-        applicationProps = new ApplicationProps();
-        applicationProps.setLocale(new Locale(LOCALE_RU));
-
-        resolver = new QuestionsFileNameResolver(messageSource, questionsFileProps,  applicationProps);
-    }
-
     @Test
     @DisplayName("имя файла корректно локализуется и возвращается")
     void testThatReturnCorrectlyLocalizedFileName() {
-        BDDMockito.given(messageSource.getMessage(questionsFileProps.getNameProperty(), new Object[]{}, applicationProps.getLocale())).
+
+        BDDMockito.given(messageSource.getMessage(QUESTION_FILE_NAME_PROPERTY,
+            new Object[]{},
+            new Locale(LOCALE_RU))).
             willReturn(QUESTION_FILE_NAME_RU);
 
         assertThat(resolver.getFileName()).isEqualTo(String.format("%s.%s", QUESTION_FILE_NAME_RU, QUESTION_FILE_EXT));
