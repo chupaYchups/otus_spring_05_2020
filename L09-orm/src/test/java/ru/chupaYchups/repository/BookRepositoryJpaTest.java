db7f180a3ce2439516e4baf2a6787fc80c812ccb
chupaYchups <chupaYchups@gmail.com>
2020-12-03 16:05:47 +0300

    homework 9

--- L09-orm/src/test/java/ru/chupaYchups/repository/BookRepositoryJpaTest.java
+++ L09-orm/src/test/java/ru/chupaYchups/repository/BookRepositoryJpaTest.java
@@ -5,28 +5,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
-import ru.chupaYchups.domain.Genre;
+import ru.chupaYchups.domain.Book;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
-@Import(GenreRepositoryJpa.class)
-//@Import(BookRepositoryJpa.class)
+@Import(BookRepositoryJpa.class)
 class BookRepositoryJpaTest {
+
+    public static final long FIRST_TEST_BOOK_ID = 1l;
 
     @Autowired
     private TestEntityManager testEntityManager;
 
     @Autowired
-    private GenreRepository genreRepository;
+    private BookRepository bookRepository;
 
     @Test
-    void save() {
-        assert genreRepository.findById(1l) != null;
-        assert testEntityManager.find(Genre.class, 1l) != null;
-
+    void testThatCorrectlyFindBookById() {
+        Book testBook = testEntityManager.find(Book.class, FIRST_TEST_BOOK_ID);
+        Optional<Book> foundBookOptional = bookRepository.findById(FIRST_TEST_BOOK_ID);
+        assertThat(foundBookOptional).isPresent().get().isEqualToComparingFieldByField(testBook);
     }
 
     @Test
-    void findById() {
+    void save() {
     }
 
     @Test
--- L09-orm/src/test/resources/testData.sql
+++ L09-orm/src/test/resources/testData.sql
@@ -1,4 +1,12 @@
 -- noinspection SqlResolveForFile
 
---insert into GENRE (id, name) values (1, 'Test genre');
-insert into GENRE (id, name) values (1, 'Detective');
+insert into GENRE (id, name) values (1, 'Detective'),
+(2, 'Novel');
+
+insert into AUTHOR(id, name) values(1,'Tolstoy'), (2,'Pushkin');
+
+insert into BOOK(name, author_id, genre_id)
+values('War and Peace', 1 , 2), ('Black man', 2 , 1);
+
+insert into COMMENT(comment_string, book_id)
+values('good book!', 1), ('excellent book!', 1), ('good book!', 2), ('excellent book!', 2);
