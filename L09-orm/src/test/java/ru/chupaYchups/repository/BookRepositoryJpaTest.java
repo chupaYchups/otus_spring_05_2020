21bb3311f74add61bf94533502eb4e81e46bdf36
chupaYchups <chupaYchups@gmail.com>
2020-12-03 22:04:33 +0300

    homework 9

--- L09-orm/src/test/java/ru/chupaYchups/repository/BookRepositoryJpaTest.java
+++ L09-orm/src/test/java/ru/chupaYchups/repository/BookRepositoryJpaTest.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import ru.chupaYchups.domain.Author;
 import ru.chupaYchups.domain.Book;
+import ru.chupaYchups.domain.Genre;
+
+import java.util.List;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,6 +18,12 @@ import static org.assertj.core.api.Assertions.*;
 class BookRepositoryJpaTest {
 
     public static final long FIRST_TEST_BOOK_ID = 1l;
+    public static final long SECOND_TEST_BOOK_ID = 1l;
+    public static final long FIRST_TEST_GENRE_ID = 1l;
+    public static final long FIRST_TEST_AUTHOR_ID = 1l;
+    public static final String TEST_AUTHOR_NAME = "test author name";
+    public static final String TEST_GENRE_NAME = "test genre name";
+    public static final String TEST_BOOK_NAME = "test book name";
 
     @Autowired
     private TestEntityManager testEntityManager;
@@ -29,18 +39,60 @@ class BookRepositoryJpaTest {
     }
 
     @Test
-    void save() {
+    void testThatCorrectlyDeleteBook() {
+        Book testBook = testEntityManager.find(Book.class, FIRST_TEST_BOOK_ID);
+        bookRepository.delete(testBook);
+        assertThat(testEntityManager.find(Book.class, FIRST_TEST_BOOK_ID)).isNull();
     }
 
     @Test
-    void delete() {
+    void testThatCorrectlyPersistNewBook() {
+        Author newAuthor = new Author(TEST_AUTHOR_NAME);
+        Genre newGenre = new Genre(TEST_GENRE_NAME);
+        Book newBook = new Book(TEST_BOOK_NAME, newAuthor, newGenre);
+
+        Book returnedBook = bookRepository.save(newBook);
+
+        assertThat(returnedBook.getId()).isNotNull();
+        assertThat(returnedBook).isEqualToIgnoringNullFields(newBook);
+
+        Book savedBook = testEntityManager.find(Book.class, returnedBook.getId());
+        assertThat(savedBook.getAuthor()).isEqualToIgnoringNullFields(newAuthor);
+        assertThat(savedBook.getGenre()).isEqualToIgnoringNullFields(newGenre);
     }
 
     @Test
-    void findBooks() {
+    void testThatCorrectlyUpdateBook() {
+        Book testBook = testEntityManager.find(Book.class, FIRST_TEST_BOOK_ID);
+        Genre newGenre = new Genre(TEST_GENRE_NAME);
+        testBook.setGenre(newGenre);
+
+        Book returnedBook = bookRepository.save(testBook);
+
+        assertThat(returnedBook).isEqualToIgnoringNullFields(testBook);
+
+        Book savedBook = testEntityManager.find(Book.class, returnedBook.getId());
+        assertThat(savedBook).isEqualToComparingFieldByField(testBook);
+    }
+
+    @Test
+    void testThatCorrectlyFindBookByParameters() {
+        Book testBook = testEntityManager.find(Book.class, FIRST_TEST_BOOK_ID);
+        Genre testBookGenre = testEntityManager.find(Genre.class, FIRST_TEST_GENRE_ID);
+        Author testBookAuthor = testEntityManager.find(Author.class, FIRST_TEST_AUTHOR_ID);
+
+        List<Book> foundBooks = bookRepository.findBooks(Optional.of(testBookAuthor), Optional.of(testBookGenre), Optional.empty());
+
+        assertThat(foundBooks).isNotEmpty().hasSize(1).contains(testBook);
     }
 
     @Test
     void getAllBooks() {
+        Book firstTestBook = testEntityManager.find(Book.class, FIRST_TEST_BOOK_ID);
+        Book secondTestBook = testEntityManager.find(Book.class, SECOND_TEST_BOOK_ID);
+
+        List<Book> foundBooks = bookRepository.getAllBooks();
+
+        assertThat(foundBooks).isNotEmpty().hasSize(2).contains(firstTestBook, secondTestBook);
     }
 }
--- L09-orm/src/test/resources/testData.sql
+++ L09-orm/src/test/resources/testData.sql
@@ -5,8 +5,8 @@ insert into GENRE (id, name) values (1, 'Detective'),
 
 insert into AUTHOR(id, name) values(1,'Tolstoy'), (2,'Pushkin');
 
-insert into BOOK(name, author_id, genre_id)
-values('War and Peace', 1 , 2), ('Black man', 2 , 1);
+insert into BOOK(id, name, author_id, genre_id)
+values(1, 'War and Peace', 1 , 1), (2, 'Black man', 2 , 1);
 
-insert into COMMENT(comment_string, book_id)
-values('good book!', 1), ('excellent book!', 1), ('good book!', 2), ('excellent book!', 2);
+insert into COMMENT(id, comment_string, book_id)
+values(1, 'good book!', 1), (2, 'excellent book!', 1), (3, 'good book!', 2), (4, 'excellent book!', 2);
